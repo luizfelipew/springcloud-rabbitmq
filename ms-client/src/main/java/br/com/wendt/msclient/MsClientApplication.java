d1aff84603c07d578ff6b1343e2f82ffe7b30ea3
luizfelipew <luizfelipewendt@gmail.com>
2022-12-12 16:46:25 -0300

    :art: feat: registering microservices in eureka server

--- ms-client/src/main/java/br/com/wendt/msclient/MsClientApplication.java
+++ ms-client/src/main/java/br/com/wendt/msclient/MsClientApplication.java
@@ -2,8 +2,10 @@ package br.com.wendt.msclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MsClientApplication {
 
 	public static void main(String[] args) {
